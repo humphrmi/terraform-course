c585f41dc16eb9dc008f7485fe55e886eac3e29a
humphrmi <44331649+humphrmi@users.noreply.github.com>
2021-11-13 20:16:06 -0600

    Update configure-remote-state.sh
    Fix his broken-ness

--- docker-demo-3/scripts/configure-remote-state.sh
+++ docker-demo-3/scripts/configure-remote-state.sh
@@ -6,9 +6,9 @@ echo 'backend.tf
 ==========
 terraform {
   backend "s3" {
-    bucket = "terraform-state-a3c731f"
+    bucket = "terraform-state-a2b6219"
     key    = "terraform.tfstate"
-    region = "eu-west-1"
+    region = "us-east-2"
   }
 }
 '
